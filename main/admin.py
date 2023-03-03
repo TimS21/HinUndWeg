c335f49383de20de38e0e71711f4f10cf91c7758
TimS21 <timschefczik@web.de>
2023-03-03 16:58:45 +0100

    Datenbankupdate

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Vehicles
 from .models import User
+from .models import Type
+from .models import Buchungstabelle
 
 # Register your models here.
 
 admin.site.register(Vehicles)
 admin.site.register(User)
+admin.site.register(Type)
+admin.site.register(Buchungstabelle)
