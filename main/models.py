a3edf16a176631ba9cf3a8e706396ab807d64258
TimS21 <timschefczik@web.de>
2023-03-03 14:19:40 +0100

    idk

--- main/models.py
+++ main/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class Vehicles(models.Model):
     id = models.IntegerField(auto_created=False, primary_key=True, serialize=False, verbose_name='ID')
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False, blank=False)
     preisProTag = models.IntegerField(default=20)
 
 class User(models.Model):
     id = models.IntegerField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    vName = models.CharField(max_length=30)
-    nName = models.CharField(max_length=30)
-    eMail = models.EmailField
-    telNr = models.IntegerField
+    vName = models.CharField(max_length=30, null=False, blank=False)
+    nName = models.CharField(max_length=30, null=False, blank=False)
+    eMail = models.EmailField(default="test@gmail.com")
+    telNr = models.IntegerField(default=1234)
 
 
 
