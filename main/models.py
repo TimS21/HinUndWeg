cbb188161433c0abe53fed88f9cd3c336ad976ad
TimS21 <timschefczik@web.de>
2023-03-03 13:44:18 +0100

    Model: User created

--- main/models.py
+++ main/models.py
@@ -5,7 +5,15 @@ from django.db import models
 class Vehicles(models.Model):
     id = models.IntegerField(auto_created=False, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(max_length=30)
-    preisProTag = models.IntegerField(default=15)
+    preisProTag = models.IntegerField(default=20)
+
+class User(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    vName = models.CharField(max_length=30)
+    nName = models.CharField(max_length=30)
+    eMail = models.EmailField
+    telNr = models.IntegerField
+
 
 
 
