f8644f41adff0be52fdb67846bafe947d58c1cff
TimS21 <63231551+TimS21@users.noreply.github.com>
2023-03-21 13:30:18 +0100

    style check

--- members/forms.py
+++ members/forms.py
@@ -4,10 +4,17 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    vorname = forms.CharField(max_length = 50)
-    nachname = forms.CharField(max_length = 50)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    vorname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    nachname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'vorname', 'nachname',  'email', 'password1', 'password2')
+    
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
