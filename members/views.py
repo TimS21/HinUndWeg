6253f6a1e90da721ebca3ec21a98a75a43dd7a18
TimS21 <63231551+TimS21@users.noreply.github.com>
2023-03-21 10:36:52 +0100

    Registrierung möglich

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     if request.method == "POST":
@@ -21,4 +22,21 @@ def logout_user(request):
     messages.success(request, ("Erfolgreich ausgeloggt!"))
     return redirect('http://127.0.0.1:8000/')
 
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!"))
+            return redirect('http://127.0.0.1:8000/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {
+		'form':form,
+		})
 
