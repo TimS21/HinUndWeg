77fdfd8fbc656ea7af1be64c95f2bbb4c4e06a7b
TimS21 <63231551+TimS21@users.noreply.github.com>
2023-03-21 14:00:49 +0100

    Test Bookingpage

--- hinUndWeg/forms.py
+++ hinUndWeg/forms.py
@@ -3,10 +3,13 @@ from django import forms
 from main.models import User
 
 class UploadForm(ModelForm):
-    Vorname = forms.TextInput()
-    Nachname = forms.TextInput()
-    EMailAdresse = forms.EmailInput()
-    telNr = forms.IntegerField()
+    Vorname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    Nachname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    EMailAdresse = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    telNr = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
         fields = ['Vorname', 'Nachname', 'EMailAdresse', 'telNr']
+    
+    def __init__(self, *args, **kwargs):
+        super(UploadForm, self).__init__(*args, **kwargs)
